da04db035324eae8b0bfa99a5b775a3a08465c92
Feo-0134 <zhujuncheng1021@gmail.com>
2019-10-19 15:09:59 +0800

    Feature: plain welcome page

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+     <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <MainPage msg = "Welcome to Your Vue.js App" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import MainPage from './components/MainPage.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    // HelloWorld,
+    MainPage
   }
 }
 </script>
