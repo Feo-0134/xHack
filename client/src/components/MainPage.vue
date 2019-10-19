155b07ee58f494ea5cc72c49fc4e34b2114b99b1
Feo-0134 <zhujuncheng1021@gmail.com>
2019-10-20 00:24:42 +0800

    Feature: Img style recognize

--- client/src/components/MainPage.vue
+++ client/src/components/MainPage.vue
@@ -2,21 +2,7 @@
   <div class="hello">
     <h1>{{ msg }}</h1>
     <div style="margin: 20px 0">
-    <div>
-        <el-button plain round>Plain</el-button>
-        <el-button type="success" plain round>Classic</el-button>
-        <el-button type="info" plain round>Rustic</el-button>
-        <el-button type="warning" plain round>Maverick</el-button>
-    </div>
-    <div>
-        <el-button type="primary" plain round>Modern Minimalist</el-button>
-        <el-button type="warning" plain round>Maverick</el-button>
-        <el-button type="danger" plain round>Retro</el-button>
-    </div>
-    <div>
-        <el-button plain round>Plain</el-button>
-        <el-button type="success" plain round>Classic</el-button>
-    </div>
+    <PickZone catagories="['hello']"/>
 </div>
 
 
@@ -24,21 +10,21 @@
 </template>
 
 <script>
+import PickZone from './PickZone.vue'
+
 export default {
   name: 'MainPage',
   props: {
     msg: String
-  }
+  },
+  components: {
+  PickZone
+}
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-.el-button.is-round {
-    border-radius: 80px !important;     
-    height: 160px !important;   
-    width: 160px !important;
-}
 h3 {
   margin: 40px 0 0;
 }
---
+++ client/src/components/PickZone.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="hello">
+   <div>
+        <el-button plain round>Plain</el-button>
+        <el-button type="warning" plain round>Maverick</el-button>
+    </div>
+    <div>
+        <el-button type="primary" plain round>Modern</el-button>
+        <el-button type="warning" plain round>Maverick</el-button>
+        <el-button type="danger" plain round>Retro</el-button>
+    </div>
+    <div>
+        <el-button plain round>Plain</el-button>
+        <el-button type="success" plain round>Classic</el-button>
+   </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PickZone',
+  props: {
+    catagories:[]
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+.el-button.is-round {
+    width: 120px;
+    height: 120px;
+    border-radius: 120px !important;  
+}
+</style>
